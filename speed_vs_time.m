7dcee944cd3c916c9520e787db4579591a4a38e5
Achi <pachi3@gatech.edu>
2019-02-01 10:16:13 -0500

    produce a plot for speed vs time per condition, display linear fit with equation on each plot

--- speed_vs_time.m
+++ speed_vs_time.m
@@ -11,15 +11,26 @@ for i = 1:36
     speeds(i,2) = speeds(i,1)/sqrt(length(speeds_array)-sum(isnan(speeds_array)));
 end
 
-% figure(1)
-% 
-% plot(time,speeds(:,1))
-% title([ctrl_or_ha,' ',num2str(concentration), ' ug/ml'])
-% xlabel(
+% Com;pute linear fit and rsq value
+linear_fit = polyfit(time',speeds(:,1),1);
+f = polyval(linear_fit,time');
+rsq = 1 - sum((speeds(:,1)-f).^2)/sum((speeds(:,1)-mean(speeds(:,1))).^2);
+
+equationString = sprintf('y = %.3g x + %.3g', linear_fit(1), linear_fit(2));
+rsqString = sprintf('R^2 = %.3g', rsq');
 
 figure
 
 errorbar(time,speeds(:,1),speeds(:,2))
+ylim([0 2])
+
+% Plot linear fit 
+hold on
+plot(time, f)
+text(10, 1.9, equationString) %max(speeds(:,1))+0.4
+text(10, 1.7, rsqString) %max(speeds(:,1))+0.2
+hold off
+
 title([ctrl_or_ha,', ',num2str(concentration), ' ug/ml Fibronectin'])
 xlabel('Time (min)')
 ylabel('Cell Speed um/min')
